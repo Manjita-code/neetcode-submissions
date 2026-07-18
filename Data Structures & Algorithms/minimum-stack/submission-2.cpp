1bdfab30ad783586d9c9fea385d5a260d2a7263c
manjita <62149436+Manjita-code@users.noreply.github.com>
2026-07-18 21:23:24 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.cpp
@@ -0,0 +1,40 @@
+class MinStack {
+   public:
+    stack<int> st;
+    stack<int> stk;
+
+
+    MinStack() {}
+
+    void push(int val) {
+        st.push(val);
+        if(stk.empty()){
+            stk.push(val);
+        }
+        else
+        {
+        stk.push(min(val, stk.top()));
+        }
+    }
+
+    void pop() {
+        if (!st.empty()) st.pop();
+        if (!stk.empty()) 
+        {
+        stk.pop();
+        stk.top();
+        }
+
+    }
+
+    int top() {
+        if (st.empty()) return -1;
+
+        return st.top();
+    }
+
+    int getMin() {
+       
+        return stk.top();
+    }
+};
